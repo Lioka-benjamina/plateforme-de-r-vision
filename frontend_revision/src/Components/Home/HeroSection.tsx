74312df92ef61014a9961984acde6a312f746143
Safidy Rakotonirina <safidyrktnr@gmail.com>
2025-08-21 14:31:18 +0300

    routes

---
+++ frontend_revision/src/Components/Home/HeroSection.tsx
@@ -0,0 +1,12 @@
+
+export default function HeroSection() {
+  return (
+    <div>
+      <div>
+        <h2>Découvrez le pouvoir du savoir avec MyRévision.</h2>
+        <p>Votre passerelle vers une éducation de qualité à Madagascar. Apprenez à votre rythme, développez vos compétences et atteignez vos objectifs académiques et professionnels.</p>
+      </div>
+      <div></div>
+    </div>
+  )
+}

5bc6a75217ac3a498335f870a235520b619651c1
Safidy Rakotonirina <safidyrktnr@gmail.com>
2025-08-28 14:34:38 +0300

    insertion logo, arrangement image heroSection, redirection btn connection

--- frontend_revision/src/Components/Home/Header.tsx
+++ frontend_revision/src/Components/Home/Header.tsx
@@ -1,11 +1,22 @@
+import { useNavigate } from "react-router-dom"
+import logo from "../../assets/images/logo/logoMyRevision.png";
 
 const Header = () => {
+  const navigate = useNavigate()
+
+  const goToLogin = () => {
+    navigate("/login")
+  }
   return (
     <div className="z-50 w-screen h-16 flex justify-between items-center bg-white  px-[5vw] shadow-md fixed top-0 left-0 right-0">
-        <div className="text-[var(--primary-color)] font-extrabold uppercase">
-            Logo
-        </div>
-        <button className="bg-[var(--primary-color)] text-white px-3 py-1 rounded-lg cursor-pointer hover:bg-blue-700 duration-300 ">Connexion</button>
+      <div className="text-[var(--primary-color)] font-extrabold uppercase">
+        <img src={logo} alt="" className="w-[5vw] "/>
+      </div>
+      <button className="bg-[var(--primary-color)] text-white px-3 py-1 rounded-lg cursor-pointer hover:bg-blue-700 duration-300 "
+        onClick={goToLogin}
+      >
+        Connexion
+      </button>
     </div>
   )
 }
--- frontend_revision/src/Components/Home/HeroSection.tsx
+++ frontend_revision/src/Components/Home/HeroSection.tsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Banner1 from "../../assets/images/banner-1.png";
 import Banner2 from "../../assets/images/banner-2.png";
 import Banner3 from "../../assets/images/banner-3.png";
 
 const HeroSection = () => {
-  const images = [Banner1, Banner2, Banner3];
+  const navigate = useNavigate()
+  const goToLogin = () =>{
+    navigate("/login")
+  }
 
   return (
     <section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-10">
       <div className="grid lg:grid-cols-2 gap-8 lg:gap-12 items-center">
         {/* Colonne gauche */}
-        <div className="flex flex-col w-full max-w-2xl">
+        <div className="flex flex-col w-full max-w-2xl mt-10 lg:mt-0">
           <h2 className="font-extrabold text-2xl sm:text-3xl lg:text-5xl leading-tight">
             Découvrez le pouvoir du savoir avec{" "}
             <span className="uppercase">MyRévision</span>.
@@ -22,24 +26,18 @@ const HeroSection = () => {
           </p>
 
           <div className="mt-6">
-            <button className="bg-[var(--primary-color)] text-white text-sm sm:text-base px-5 py-3 rounded-lg hover:bg-blue-700 duration-300 transition cursor-pointer">
+            <button className="bg-[var(--primary-color)] text-white text-sm sm:text-base px-5 py-3 rounded-lg hover:bg-blue-700 duration-300 transition cursor-pointer" onClick={goToLogin}>
               Commencer votre parcours
             </button>
           </div>
         </div>
 
         {/* Colonne droite : grille d'images responsive */}
-        <div className="mt-4 lg:mt-0">
-          <div className="grid grid-cols-3 gap-3 sm:gap-4 lg:pr-[5vw]">
-            {images.map((img, index) => (
-              <img
-                key={index}
-                src={img}
-                alt={`Banner ${index + 1}`}
-                className="w-full aspect-[3/4] object-cover rounded-xl shadow-md"
-                loading="lazy"
-              />
-            ))}
+        <div className="hidden lg:block mt-4 ml-10">
+          <div className="relative grid grid-cols-3 gap-3 sm:gap-4 lg:pr-[5vw] ">
+            <img src={Banner1} alt="" className="relative -top-20 left-15 z-4"/>
+            <img src={Banner2} alt="" className="z-3"/>
+            <img src={Banner3} alt="" className="relative top-20 -left-15 z-2"/>
           </div>
         </div>
       </div>
